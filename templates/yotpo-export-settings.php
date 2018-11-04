2749d53ff079fa43c31a283681595b49adfe1803
Lior Gingi <lior.lgh@gmail.com>
2018-11-04 23:04:50 +0200

    Update yotpo-export-settings.php

--- templates/yotpo-export-settings.php
+++ templates/yotpo-export-settings.php
@@ -9,18 +9,18 @@ function admin_ui(){
 		<div class='step'>
 			<div class='step-container'>
 				<h3>Step 1: <i>(Optional)</i></h3>
-				<input type='checkbox' name='private' value='Yes'>Include products that are <i>private</i>
+				<input type='checkbox' name='private' value='Yes'>Include products that their <i>visibility</i> is set to <b>Private</b>
 				</br></br>
-				<input type='checkbox' name='draft' value='Yes'>Include products that their status is <i>Draft</i>
+				<input type='checkbox' name='draft' value='Yes'>Include products that their <i>status</i> is set to <b>Draft</b>
 				</br></br>
-				<input type='checkbox' name='pending' value='Yes'>Include products that their status is <i>Pending Review</i>
+				<input type='checkbox' name='pending' value='Yes'>Include products that their <i>status</i> is set to <b>Pending Review</b>
 			</div>
 		</div>
 		</br></br>
-		<div class='step'>
+		<div class='step'> 
 			<div class='step-container'>
 				<h3>Step 2: Unique Identifiers <i>(Optional)</i></h3>
-				<p class='instructions'>If you're using one of the following plguins for unique identifiers, please choose the one that you're using:</p>
+				<p class='instructions'>If you're using one of the following plugins, please choose the plugin in use: </p>
 				<input type='radio' name='plugin' value='wc-uei'>WooCommerce UPC, EAN, and ISBN
 				</br></br>
 				<input type='radio' name='plugin' value='pgtin-wc'>Product GTIN (EAN, UPC, ISBN) for WooCommerce
@@ -30,13 +30,13 @@ function admin_ui(){
 				<input type='radio' name='plugin' value='wc-upf'>Ultimate Products Feed
 				</br></br>
 				<hr>
-				<p style='font-size:20px; text-align:center; margin: 0 auto;'><b>OR</b></p>
+				<p style='font-size:20px; margin: auto 50px;'><b>OR</b></p>
 				<hr>
-				<p class='instructions'>If you're using custom meta-fields, please fill the relevant text-boxes below:</p>
+				<p class='instructions'>If you're using custom meta-fields, please fill the relevant fields below:</p>
 				<div class='customization'>GTIN (UPC/ISBN)</div> <input type='text' name='cust_gtin'><br><br>
 				<div class='customization'>MPN</div> <input type='text' name='cust_mpn'><br><br>
 				<div class='customization'>Brand</div> <input type='text' name='cust_brand'><br><br>
-				<p><b>*Please note that Yotpo supports only UPC-A, ISBN and MPN+Brand.</b></p>
+				<p><i><b>*Please note that Yotpo supports only UPC-A, ISBN and MPN+Brand.</b></i></p>
 			</div>
 		</div>
 		</br></br>
